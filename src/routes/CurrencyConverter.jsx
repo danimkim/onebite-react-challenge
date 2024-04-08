b1ce82ed7a944c3fdb2908405de06fc53c73fd37
dahhnym <danimkim.dev@gmail.com>
2024-04-08 22:39:08 +0900

    feat: implement currency converter

--- src/routes/CurrencyConverter.jsx
+++ src/routes/CurrencyConverter.jsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./CurrencyConverter.module.css";
+import { CURRENCY } from "../constants/constant";
 
-const CurrencyInput = ({ currency }) => {
+const CurrencyInput = ({ currencyCode, InputChangeHandler, inputValue }) => {
   return (
     <div className={styles.InputLabel}>
       <label htmlFor="" className={styles.InputLabel}>
-        {currency}
+        {currencyCode}
       </label>
       <input
         type="number"
         className={styles.Input}
         placeholder="숫자를 입력하세요"
+        min="0"
+        onChange={(e) => InputChangeHandler(e.target.value, currencyCode)}
+        value={inputValue}
       />
     </div>
   );
 };
 
 export default function CurrencyConverter() {
+  const [currencyValue, setCurrencyValue] = useState();
+
+  const handleInputChange = (value, currencyCode) => {
+    if (currencyCode === CURRENCY.SOUTH_KOREA.CURRENCY_CODE) {
+      setCurrencyValue({
+        krw: value,
+        usd: value / CURRENCY.SOUTH_KOREA.CURRENCY_RATE,
+      });
+    } else {
+      setCurrencyValue({
+        krw: value * CURRENCY.SOUTH_KOREA.CURRENCY_RATE,
+        usd: value,
+      });
+    }
+  };
+
   return (
     <div className={styles.Container}>
       <h1>환율 변환기</h1>
       <form className={styles.Form}>
-        <CurrencyInput currency="KRW" />
-        <CurrencyInput currency="USD" />
+        <CurrencyInput
+          currencyCode={CURRENCY.SOUTH_KOREA.CURRENCY_CODE}
+          InputChangeHandler={handleInputChange}
+          inputValue={currencyValue?.krw ?? ""}
+        />
+        <CurrencyInput
+          currencyCode={CURRENCY.UNITED_STATES.CURRENCY_CODE}
+          InputChangeHandler={handleInputChange}
+          inputValue={currencyValue?.usd ?? ""}
+        />
       </form>
     </div>
   );
