2bbe059a074bc2c74d64cf0377a4db33ea208dcd
dahhnym <danimkim.dev@gmail.com>
2024-04-08 16:05:14 +0900

    feat: set up page routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root.jsx";
+import ErrorPage from "./error-page.jsx";
+import Day10 from "./routes/Day10.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/day10",
+    element: <Day10 />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/root.jsx
@@ -0,0 +1,17 @@
+export default function Root() {
+  return (
+    <>
+      <h1>Challenge Mission List</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href={`/day10`}>Day10</a>
+          </li>
+          <li>
+            <a href={`/day13`}>Day13</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
