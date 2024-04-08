5b49212a826d4beba5d5d8f5bb78439631235371
dahhnym <danimkim.dev@gmail.com>
2024-04-09 00:48:14 +0900

    fix: modify link directory

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,22 +1,22 @@
 import { Link } from "react-router-dom";
+import { BASE_URL } from "../constants/constant";
 
 export default function Root() {
-  const BASE_URL = `/onebite-react-challenge`;
   return (
     <>
       <h1>Challenge Mission List</h1>
       <nav>
         <ul>
           <li>
-            <Link to={`${BASE_URL}/mission/day10`}>Day10</Link>
+            <Link to={`/${BASE_URL}/mission/day10`}>Day10</Link>
           </li>
           <li>
-            <Link to={`${BASE_URL}/mission/day13`}>Day13</Link>
+            <Link to={`/${BASE_URL}/mission/day13`}>Day13</Link>
           </li>
         </ul>
         <ul>
           <li>
-            <Link to={`${BASE_URL}/project/counter`}>Counter App</Link>
+            <Link to={`/${BASE_URL}/project/counter`}>Counter App</Link>
           </li>
         </ul>
       </nav>
