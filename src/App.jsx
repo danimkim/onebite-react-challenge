dd040180e5558e65d9a1913ff53c777438e87cc5
dahhnym <danimkim.dev@gmail.com>
2024-04-08 16:08:55 +0900

    refactor: create new component for day10 mission

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,5 @@
-import { useState } from "react";
-import "./App.css";
-
-function Welcome({ name, isMember }) {
-  const WELCOME_BACK_MESSAGE = "다시 오셨군요";
-  const REGISTER_MESSAGE = "가입하시겠어요?";
-
-  return (
-    <p>{`${name}님 ${isMember ? WELCOME_BACK_MESSAGE : REGISTER_MESSAGE}`}</p>
-  );
-}
-
 function App() {
-  const [hasSignedIn, setHasSignedIn] = useState(false);
-
-  const userInfo = {
-    name: "이정환",
-    isMember: hasSignedIn,
-  };
-
-  const handleClick = () => setHasSignedIn((prev) => !prev);
-
-  return (
-    <>
-      <Welcome {...userInfo} />
-      <button type="button" onClick={handleClick}>
-        {hasSignedIn ? "로그아웃" : "회원가입"}
-      </button>
-    </>
-  );
+  return <div>Onebite React Challenge</div>;
 }
 
 export default App;
-
-Welcome.defaultProps = {
-  name: "방문자",
-  isMember: false,
-};
---
+++ src/routes/day10.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function Welcome({ name, isMember }) {
+  const WELCOME_BACK_MESSAGE = "다시 오셨군요";
+  const REGISTER_MESSAGE = "가입하시겠어요?";
+
+  return (
+    <p>{`${name}님 ${isMember ? WELCOME_BACK_MESSAGE : REGISTER_MESSAGE}`}</p>
+  );
+}
+
+export default function Day10() {
+  const [hasSignedIn, setHasSignedIn] = useState(false);
+
+  const userInfo = {
+    name: "이정환",
+    isMember: hasSignedIn,
+  };
+
+  const handleClick = () => setHasSignedIn((prev) => !prev);
+
+  return (
+    <>
+      <Welcome {...userInfo} />
+      <button type="button" onClick={handleClick}>
+        {hasSignedIn ? "로그아웃" : "회원가입"}
+      </button>
+    </>
+  );
+}
